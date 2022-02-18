5321acd845a386cf397ce7812179139733dad946
nhat20t1020495husc <99929596+nhat20t1020495husc@users.noreply.github.com>
2022-02-18 18:12:28 +0700

    Add files via upload

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Nhat
- */
+
 public class Welcome {
-    
+    public static void main(String []args){
+        System.out.println("Welcome to Java! ");
+    }
 }
+
