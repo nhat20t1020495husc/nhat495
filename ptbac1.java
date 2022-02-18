c695ea489aa4a3976d769e86aef21141df4b5754
nhat20t1020495husc <99929596+nhat20t1020495husc@users.noreply.github.com>
2022-02-18 10:40:34 +0700

    Add files via upload

---
+++ ptbac1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+import java.util.Scanner;
+public class ptbac1 {
+     public static void main(String []args)
+    {
+    Scanner pt=new Scanner(System.in);
+    double a,b;
+    try
+    {
+      System.out.println(" Nhap a : ");
+      System.out.println(" Nhap b : ");
+   
+      if(a==0) {
+        if (b==0) System.out.println("PT vo so nghiem ");
+        if (b!=0) System.out.println("PT vo nghiem ");
+    }
+    else
+        System.out.println("PT co nghiem " +(-b/a));
+     }catch(Exception e)
+        {
+            System.out.println(e);
+      }
+}
+
